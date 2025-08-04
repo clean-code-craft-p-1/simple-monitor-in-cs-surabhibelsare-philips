b85b3baa389ef1044e3d673babddbae44a811bb2
surabhi <surabhi.belsare@philips.com>
2025-08-04 10:59:22 +0530

    removing duplication

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace checker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Checking vitals...");
+            bool result = Checker.VitalsOk(98.6f, 75, 95);
+            if (result)
+            {
+                Console.WriteLine("All vitals are within normal range.");
+            }
+            else
+            {
+                Console.WriteLine("Some vitals are out of range.");
+            }
+        }
+    }
+}
--- checker.cs
+++ checker.cs
@@ -8,41 +8,34 @@ class Checker
         if(temperature >102 || temperature < 95)
         {
             Console.WriteLine("Temperature critical!");
-            for (int i = 0; i < 6; i++)
-            {
-                Console.Write("\r* ");
-                System.Threading.Thread.Sleep(1000);
-                Console.Write("\r *");
-                System.Threading.Thread.Sleep(1000);
-            }
+            SleepForAWhile();
             return false;
         }
         else if (pulseRate < 60 || pulseRate > 100)
         {
             Console.WriteLine("Pulse Rate is out of range!");
-            for (int i = 0; i < 6; i++)
-            {
-                Console.Write("\r* ");
-                System.Threading.Thread.Sleep(1000);
-                Console.Write("\r *");
-                System.Threading.Thread.Sleep(1000);
-            }
+            SleepForAWhile();
             return false;
         }
         else if (spo2 < 90)
         {
             Console.WriteLine("Oxygen Saturation out of range!");
-            for (int i = 0; i < 6; i++)
-            {
-                Console.Write("\r* ");
-                System.Threading.Thread.Sleep(1000);
-                Console.Write("\r *");
-                System.Threading.Thread.Sleep(1000);
-            }
+            SleepForAWhile();
             return false;
         }
         Console.WriteLine("Vitals received within normal range");
         Console.WriteLine("Temperature: {0} Pulse: {1}, SO2: {2}", temperature, pulseRate, spo2);
         return true;
+    }
+
+    private static void SleepForAWhile()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            Console.Write("\r* ");
+            System.Threading.Thread.Sleep(1000);
+            Console.Write("\r *");
+            System.Threading.Thread.Sleep(1000);
+        }
     }
 }
